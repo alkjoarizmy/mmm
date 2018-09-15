9f21872dd0ccff3dc732d3e97b71c242c52e0df8
alkjoarizmy <Gymnastiar12345678@gmail.com>
2018-09-16 06:32:48 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$channelAccessToken = 'CHANNEL ACCESS TOKEN';
+$channelAccessToken = 'Si31A4TZYFzZzjU/N4YVAw7Us3CdqRcTeRuTRGMFUTVPlxGZLv9V51aoDb+npIkxDvfi/Dm9smuxrZUB4XYRNCaJWzs9svaW8HEmO7kyb5fnxMojKbKw3om6ZekiPC4iY07a2OkLn1AUktakEIlUGAdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = 'CHANNEL SECRET';
+$channelSecret = 'a30657e29705f49023b7967e26089e76';
